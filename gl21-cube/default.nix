a002fbba2561053a7c60ebe0a64e37f33e154aaf
Philipp Sauter <sauterp@protonmail.com>
2021-03-06 17:18:45 +0100

    the build of the example completes, however the execution fails

--- gl21-cube/default.nix
+++ gl21-cube/default.nix
@@ -8,7 +8,8 @@ with import <nixpkgs> {}; {
 			# We don't want to update the SHA everytime the dependencies are updated during development, thus null.
 			vendorSha256 = null;
 
-			buildInputs = [ stdenv pkgconfig xorg.libX11.dev xorg.libXcursor.dev xorg.libXrandr.dev xorg.libXinerama.dev xorg.libXi.dev xlibs.libXext.dev libGL.dev xorg.libXxf86vm.dev ];
+			nativeBuildInputs = [ pkgconfig gcc ];
+			buildInputs = [ stdenv go gcc xorg.libX11.dev xorg.libXcursor.dev xorg.libXrandr.dev xorg.libXinerama.dev xorg.libXi.dev xlibs.libXext.dev libGL.dev xorg.libXxf86vm.dev ];
 
 			subPackages = [ "./gl21-cube/cube.go" ];
 
